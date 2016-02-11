bd0278bceee9c6e0e1a365b1ffdcef5da08afbf1
Rob Ede <robjtede@gmail.com>
2016-02-11 21:16:44 +0000

    added request and retrieve to Warehouse

--- java/Warehouse.java
+++ java/Warehouse.java
@@ -8,4 +8,22 @@ public class Warehouse extends Location {
 		this.stock = stock;
 	}
 	
+	public boolean request (int[] products) {
+		for (int i=0, j=available.length; i<j; i++) {
+			if (available[i] - products[i] < 0) return false;
+			available[i] -= products[i];
+		}
+		
+		return true;
+	}
+	
+	public boolean retrieve (int[] products) {
+		for (int i=0, j=available.length; i<j; i++) {
+			if (stock[i] - products[i] < 0) return false;
+			available[i] -= products[i];
+		}
+		
+		return true;
+	}
+	
 }
