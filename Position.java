7bcf54bb35d213de6ea4200dbafd165908a38c59
Rob Ede <robjtede@gmail.com>
2016-02-11 20:44:17 +0000

    added Position.java

---
+++ Position.java
@@ -0,0 +1,32 @@
+public class Position {
+	
+	private int row = 0;
+	private int col = 0;
+	
+	public Position (row, col) {
+		this.row = row;
+		this.col = col;
+	}
+	
+	public int getRow () {
+		return this.row;
+	}
+	
+	public void setRow (int row) {
+		this.row = row;
+	}
+	
+	public int getCol () {
+		return this.col;
+	}
+	
+	public void setCol (int col) {
+		this.col = col;
+	}
+	
+	
+	public boolean equals (Position pos) {
+		return (this.row == pos.row) && (this.col == pos.col);
+	}
+	
+}
