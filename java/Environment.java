9358cd79deeb60ce4f6298058794d3116dd7c810
Blayze Millward <blayze.millward@googlemail.com>
2016-02-11 21:10:47 +0000

    Added drone stuff

---
+++ java/Environment.java
@@ -0,0 +1,12 @@
+public class Environment {
+	public Location[][] map;
+
+	public Environment ()
+	{
+
+	}
+	public void deliver (Drone d)
+	{
+		map[d.getPosition().getRow()][d.getPosition().getCol()].
+	}
+}
