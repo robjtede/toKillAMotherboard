86bcf629ad150495075eafc73a153f3871800af6
Rob Ede <robjtede@gmail.com>
2016-02-11 19:27:55 +0000

    added drone class and sim class

--- commands.py
+++ commands.py
@@ -1,11 +1,13 @@
 def load(drone, store, item_id, item_qt):
-	sim['drones'][drone]['products'][item_id] += item_qt
-	sim['drones'][drone]['commands'] = 'load'
-	sim['drones'][drone]['time'] = 1
-	
+    sim['drones'][drone]['products'][item_id] += item_qt
+    sim['drones'][drone]['commands'] = 'load'
+    sim['drones'][drone]['time'] = 1
+    
 def deliver():
-	
+    pass
+
 def unload():
-	
+    pass
+
 def wait(droneNumber, time):
-	sim['drones'][droneNumber]['timer'] = time
+    sim['drones'][droneNumber]['timer'] = time
---
+++ drone.py
@@ -0,0 +1,10 @@
+class Drone:    
+    def __init__(self, pos):
+        self.pos = pos
+        self.timer = 0
+        self.action = 'none'
+    
+    load
+
+
+print Drone({'row': 0, 'col': 0}).
