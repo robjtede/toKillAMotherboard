76d03e883d39d635f5583d3f10bb544d86adfb5a
endom8rix <mbarber2@sheffield.ac.uk>
2016-02-11 19:29:46 +0000

    Modified: Changed Load Function

--- commands.py
+++ commands.py
@@ -1,7 +1,5 @@
 def load(drone, store, item_id, item_qt):
 	sim['drones'][drone]['products'][item_id] += item_qt
-	sim['drones'][drone]['commands'] = 'load'
-	sim['drones'][drone]['time'] = 1
 	
 def deliver():
 	
