873e09859f355daea0373bfe19846b1a00548d9e
endom8rix <mbarber2@sheffield.ac.uk>
2016-02-11 19:36:46 +0000

    Modified: Changed Load and Deliver Functions

--- commands.py
+++ commands.py
@@ -1,8 +1,11 @@
 def load(drone, store, item_id, item_qt):
 	sim['drones'][drone]['products'][item_id] += item_qt
+	sim['warehouses'][store]['products'][item_id] -= item_qt
 	
-def deliver():
-    pass
+    
+def deliver(drone, store):
+    sim['drones'][drone]['pos']['row'] = sim['warehouses'][store]['pos']['row']
+    sim['drones'][drone]['pos']['col'] = sim['warehouses'][store]['pos']['col']
 
 def unload():
     pass
