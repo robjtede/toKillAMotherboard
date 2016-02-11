c0bbd74af08861741d3d773014313ecafd46fbc7
Rob Ede <robjtede@gmail.com>
2016-02-11 19:48:09 +0000

    updated drone.py to reference self

--- drone.py
+++ drone.py
@@ -1,19 +1,20 @@
-class Drone:    
+class Drone:
     def __init__(self, pos):
         self.pos = pos
         self.timer = 0
-        self.action = 'none'
+        self.action = lambda: pass
         self.items = []
     
     def load(store, item_id, item_qt):
-        sim['drones'][drone]['products'][item_id] += item_qt
+        self.items['item_id'] += item_qt
+        sim['warehouses'][store]['products'][item_id] -= item_qt
 
-    def deliver():
-        pass
+    def deliver(store):
+        self.pos = sim['orders'][store]['pos']
 
     def unload(store, item_id, item_qt):
-        sim['drones'][drone]['products'][item_id] -= item_qt
+        self.items['item_id'] -= item_qt
 
     def wait(time):
-        sim['drones'][droneNumber]['timer'] = time
+        self.timer = time
     
